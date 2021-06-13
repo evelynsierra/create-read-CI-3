e337886802208041febc072757b89c0c708610fc
Sierra <55088939+evelynsierra@users.noreply.github.com>
2021-06-13 23:39:38 +0700

    init

---
+++ application/models/Siswa_model.php
@@ -0,0 +1,21 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+class Siswa_model extends CI_Model
+{
+    private $table = "tb_siswa";
+
+    public function getAll()
+    {
+        return $this->db->get($this->table)->result();
+    }
+
+    public function save($data)
+    {
+        return $this->db->insert($this->table, $data);
+    }
+
+    public function getById($id)
+    {
+        return $this->db->get_where($this->table, ["kd_siswa" => $id])->row();
+    }
+}
---
+++ application/views/template/footer.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container" style="text-align:center;">
+        <hr />
+        <span>Evelyn Sierra @<?php echo date('Y'); ?></span>
+    </div>
+</footer>
+</body>
+
+</html>
